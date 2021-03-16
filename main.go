80cc7a010746382206e9b162d317d7af216009f7
sj <sebastianthegreatful@gmail.com>
2021-03-17 02:48:09 +0700

    Get latest v* tag

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+	tag := execute("git", "describe", "--tags", "--match", "v*", "--abbrev=0")
+	fmt.Println(tag)
+}
+
+func execute(param ...string) string {
+	app := param[0]
+
+	cmd := exec.Command(app, param[1:]...)
+	stdout, err := cmd.Output()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return ""
+	}
+
+	return strings.TrimSpace(string(stdout))
 }
