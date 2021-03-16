bb91d5131f779d91a7ecda826b4a825dc67c6b5e
sj <sebastianthegreatful@gmail.com>
2021-03-17 03:07:24 +0700

    Bump version

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tonsV2/git-release
 
 go 1.14
+
+require github.com/blang/semver/v4 v4.0.0
--- main.go
+++ main.go
@@ -2,17 +2,30 @@ package main
 
 import (
 	"fmt"
+	"github.com/blang/semver/v4"
 	"os/exec"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
 	tag := execute("git", "describe", "--tags", "--match", "v*", "--abbrev=0")
-	fmt.Println(tag)
+	fmt.Printf("Current version: %s\n", tag)
+
+	version, err := semver.Make(tag[1:])
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	_ = version.IncrementMinor()
+	nextTag := fmt.Sprintf("v%s", version)
+
+	fmt.Printf("Next version: %s\n", nextTag)
 }
 
 func execute(param ...string) string {
+	fmt.Println(strings.Join(param, " "))
+
 	app := param[0]
 
 	cmd := exec.Command(app, param[1:]...)
