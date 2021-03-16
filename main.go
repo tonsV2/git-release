350b11be9eba45420b97b0d2df2049366a4563ab
sj <sebastianthegreatful@gmail.com>
2021-03-17 03:19:40 +0700

    Refactor main function into smaller parts

--- main.go
+++ main.go
@@ -8,22 +8,43 @@ import (
 )
 
 func main() {
-	tag := execute("git", "describe", "--tags", "--match", "v*", "--abbrev=0")
-	fmt.Printf("Current version: %s\n", tag)
+	currentVersion := findCurrentVersion()
+	fmt.Printf("Current version: %s\n", currentVersion)
 
+	nextVersion, done := nextVersion(currentVersion)
+	if done {
+		return
+	}
+	fmt.Printf("Next version: %s\n", nextVersion)
+
+	gitTag(nextVersion)
+
+	gitPushTags()
+}
+
+func gitPushTags() string {
+	return execute("git", "push", "--tags")
+}
+
+func gitTag(nextTag string) string {
+	return execute("git", "tag", nextTag)
+}
+
+func nextVersion(tag string) (string, bool) {
 	version, err := semver.Make(tag[1:])
 	if err != nil {
 		fmt.Println(err.Error())
-		return
+		return "", true
 	}
 
 	_ = version.IncrementMinor()
-	nextTag := fmt.Sprintf("v%s", version)
+	nextVersion := fmt.Sprintf("v%s", version)
+	return nextVersion, false
+}
 
-	fmt.Printf("Next version: %s\n", nextTag)
-
-	execute("git", "tag", nextTag)
-	execute("git", "push", "--tags")
+func findCurrentVersion() string {
+	tag := execute("git", "describe", "--tags", "--match", "v*", "--abbrev=0")
+	return tag
 }
 
 func execute(param ...string) string {
